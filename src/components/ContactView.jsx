009de1b70145bbb06b80fb5f26724c2d27fd8392
JakeBrowning90 <jacobbrowning90@gmail.com>
2025-04-09 21:39:26 -0400

    Add placeholder text

--- src/components/ContactView.jsx
+++ src/components/ContactView.jsx
@@ -12,7 +12,16 @@ function ContactView(
   return (
     <div>
       <h1>Contact View</h1>
+      <p>
+        Interested in working together? Feel free to email me or contact me on
+        social media.
+      </p>
+      <p>Email:</p>
+
       <p>Links to social media</p>
+      <p>LinkedIn</p>
+      <p>Bluesky</p>
+      <p>GitHub</p>
     </div>
   );
 }
--- src/components/HomeView.jsx
+++ src/components/HomeView.jsx
@@ -11,9 +11,22 @@ function HomeView(
   // Render
   return (
     <div>
-      <h1>Home View</h1>
+      <h1>Jake Browning</h1>
+      <h2>Web Developer</h2>
       <p>Banner image</p>
-      <p>Introduction</p>
+      <p>
+        My name is Jake, and I love solving puzzles, simplifying complex
+        concepts, and creating beautiful things.
+      </p>
+      <p>
+        I started my web development career with an elective course, and have
+        since cultivated my skills through IT studies, professional networking,
+        and self-teaching with online resources.
+      </p>
+      <p>
+        See more of my career experience on LinkedIn and my coding on GitHub.
+      </p>
+
       <p>Tech stack</p>
     </div>
   );
