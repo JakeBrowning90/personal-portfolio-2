085582d2c385dc2e1d22fbd2ace81381b8603ccd
JakeBrowning90 <jacobbrowning90@gmail.com>
2025-04-12 16:23:09 -0400

    Add centered text div to home view

--- src/components/HomeView.jsx
+++ src/components/HomeView.jsx
@@ -12,25 +12,26 @@ function HomeView(
   // Render
   return (
     <div>
-      <h1>Jake Browning</h1>
-      <h2>Web Developer</h2>
-      <p>Hero image</p>
-      <p>
-        My name is Jake, and I love solving puzzles, simplifying complex
-        concepts, and creating beautiful things.
-      </p>
-      <p>
-        I started my web development career with an elective course, and have
-        since cultivated my skills through IT studies, professional networking,
-        and self-teaching with online resources.
-      </p>
-      <p>
-        See more of my career experience on LinkedIn and my coding on GitHub.
-      </p>
+      <div className="px-4 py-5 my-5 text-center shadow">
+        <h1>Jake Browning</h1>
+        <h2>Web Developer</h2>
+        <p>
+          My name is Jake, and I love solving puzzles, simplifying complex
+          concepts, and creating beautiful things.
+        </p>
+        <p>
+          I started my web development career with an elective course, and have
+          since cultivated my skills through IT studies, professional
+          networking, and self-teaching with online resources.
+        </p>
+        <p>
+          See more of my career experience on LinkedIn and my coding on GitHub.
+        </p>
+      </div>
 
       <p>Tech stack</p>
       {techStackList.map((icon) => {
-        return <img src={icon} alt="" className="img-thumbnail"/>;
+        return <img src={icon} alt="" className="img-thumbnail" />;
       })}
     </div>
   );
