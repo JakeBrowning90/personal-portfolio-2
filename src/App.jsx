bcf5e0573c96890caf06f622975e04126311d7fe
JakeBrowning90 <jacobbrowning90@gmail.com>
2025-04-13 18:02:12 -0400

    Add collapsable navbar

--- src/App.jsx
+++ src/App.jsx
@@ -9,20 +9,36 @@ import Error404View from "./components/Error404View";
 function App() {
   return (
     <>
-      <nav className="navbar sticky-top navbar-expand-lg bg-dark">
-        <div className="container-fluid collapse navbar-collapse">
-          <Link to="/" className="nav-link text-light">
-            Home
-          </Link>
-          <Link to="/about" className="nav-link text-light">
-            About
-          </Link>
-          <Link to="/projects" className="nav-link text-light">
-            Projects
-          </Link>
-          <Link to="/contact" className="nav-link text-light">
-            Contact
-          </Link>
+      <nav
+        className="navbar navbar-expand-lg navbar-light bg-dark"
+        data-bs-theme="dark"
+      >
+        <Link to="/" className="nav-link text-light">
+          Home
+        </Link>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNavAltMarkup"
+          aria-controls="navbarNavAltMarkup"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon text-light"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+          <div className="navbar-nav">
+            <Link to="/about" className="nav-item nav-link text-light">
+              About
+            </Link>
+            <Link to="/projects" className="nav-item nav-link text-light">
+              Projects
+            </Link>
+            <Link to="/contact" className="nav-item nav-link text-light">
+              Contact
+            </Link>
+          </div>
         </div>
       </nav>
       <main>
