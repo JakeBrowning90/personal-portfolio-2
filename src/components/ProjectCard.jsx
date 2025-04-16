85a8ae29f8f61f89589b9d177a0bb9ab99528edd
JakeBrowning90 <jacobbrowning90@gmail.com>
2025-04-16 13:50:07 -0400

    Add responsive columns to project detail page

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -16,13 +16,11 @@ function ProjectCard({ project }) {
     <div className="col">
       <div className="card h-100">
         <img src={project.thumbnail} alt="" className="card-img-top h-100" />
-        <div className="card-body">
+        <div className="card-body text-center">
           <Link to={`/projects/${project.id}`}>
-            <p className="card-title">{project.title}</p>
+            <p className="card-title fw-bold">{project.title}</p>
           </Link>
           {/* <p>{project.desc}</p> */}
-          <a href={project.liveLink}>Live</a>
-          <a href={project.repoLink}>Repo</a>
         </div>
       </div>
     </div>
--- src/components/ProjectDetail.jsx
+++ src/components/ProjectDetail.jsx
@@ -18,14 +18,31 @@ function ProjectDetail(
 
   // Render
   return (
-    <div>
-      <Link to="/projects">Back</Link>
-      <h1>Project Detail</h1>
-      <h1>{targetProject.title}</h1>
-      <p>{targetProject.desc}</p>
-      <p>{targetProject.liveLink}</p>
-      <p>{targetProject.repoLink}</p>
-      <img src={targetProject.thumbnail} alt="" />
+    <div className="container px-4 py-5 shadow-lg">
+      {/* <h1>Project Detail</h1> */}
+      <div className="row flex-leg-row-reverse g-5 py-5">
+        <div className="col-lg-6">
+          <Link to="/projects">Back</Link>
+          <h1>{targetProject.title}</h1>
+          <p>{targetProject.desc}</p>
+
+          <a
+            href={targetProject.liveLink}
+            className="mx-1 my-1 px-1 py-1 rounded bg-primary text-light"
+          >
+            Live Site
+          </a>
+          <a
+            href={targetProject.repoLink}
+            className="mx-1 my-1 px-1 py-1 rounded bg-primary text-light"
+          >
+            GitHub Repo
+          </a>
+        </div>
+        <div className="col-10 col-sm-8 col-lg-6">
+          <img src={targetProject.thumbnail} alt="" />
+        </div>
+      </div>
     </div>
   );
 }
